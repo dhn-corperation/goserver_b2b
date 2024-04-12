12104d751dadf1789832fcdf9fd0c462f48d5b11
dohwe0528@gmail.com <dohwe0528@gmail.com>
2024-04-12 15:44:07 +0900

    modified dhncenter.go

--- src/kaocenter/dhncenter.go
+++ src/kaocenter/dhncenter.go
@@ -4,22 +4,8 @@ import (
 	config "mycs/src/kaoconfig"
 	db "mycs/src/kaodatabasepool"
 	"database/sql"
-	"net"
-	"net/http"
-	"time"
 	"github.com/gin-gonic/gin"
 )
-
-var centerClient *http.Client = &http.Client{
-	Timeout: time.Second * 30,
-	Transport: &http.Transport{
-		Dial: (&net.Dialer{
-			Timeout:   30 * time.Second,
-			KeepAlive: 30 * time.Second,
-		}).Dial,
-		TLSHandshakeTimeout: 10 * time.Second,
-	},
-}
 
 func Get_crypto(c *gin.Context){
 	userid := c.Request.Header.Get("userid")
@@ -28,7 +14,7 @@ func Get_crypto(c *gin.Context){
 	err := db.DB.QueryRow("select crypto from DHN_CLIENT_LIST where user_id = '"+userid+"' and ip = '"+userip+"'").Scan(&crypto)
 	if err != nil { config.Stdlog.Println(err) }
 	if crypto.Valid {
-		c.String(200, crypto)
+		c.String(200, crypto.String)
 	} else {
 		c.String(404, "조건에 맞는 결과값이 없습니다.")
 	}
