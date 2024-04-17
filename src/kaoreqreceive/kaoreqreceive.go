464ffd90bea9334b370ab268153813861efc946d
dohwe0528@gmail.com <dohwe0528@gmail.com>
2024-04-17 13:30:17 +0900

    first

--- src/kaoreqreceive/kaoreqreceive.go
+++ src/kaoreqreceive/kaoreqreceive.go
@@ -160,7 +160,8 @@ func ReqReceive(c *gin.Context) {
 				ftValue.Send_group = nil
 				ftValue.Supplement = msg[i].Supplement
 				if len(msg[i].Price) > 0 {
-					price, _ := int64(strconv.Atoi(msg[i].Price))
+					price, _ := strconv.Atoi(msg[i].Price)
+					price, _ := int64(price)
 					ftValue.Price = &price
 				} else {
 					ftValue.Price = nil
@@ -307,7 +308,8 @@ func ReqReceive(c *gin.Context) {
 				atValue.Send_group = nil
 				atValue.Supplement = msg[i].Supplement
 				if len(msg[i].Price) > 0 {
-					price, _ := int64(strconv.Atoi(msg[i].Price))
+					price, _ := strconv.Atoi(msg[i].Price)
+					price, _ := int64(price)
 					atValue.Price = &price
 				} else {
 					atValue.Price = nil
