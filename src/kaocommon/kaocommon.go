d12428e639470e6ee65d6000c12d298fcdc28276
dohwe0528@gmail.com <dohwe0528@gmail.com>
2024-04-17 10:26:07 +0900

    first

--- src/kaocommon/kaocommon.go
+++ src/kaocommon/kaocommon.go
@@ -289,7 +289,7 @@ func InitDatabaseColumn(columnTypes []*sql.ColumnType, length int) []interface{}
 func GetReqColumnPq(mtype interface{}) []string {
 	t := reflect.TypeOf(mtype)
 	columns := make([]string, t.NumField())
-	for i:=0;i<t.Numfield();i++{
+	for i:=0;i<t.NumField();i++{
 		columns[i] = t.Field(i).Tag.Get("db")
 	}
 	return columns
