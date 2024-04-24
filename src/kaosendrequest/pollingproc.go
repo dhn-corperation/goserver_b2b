6ba88a47b7d262b1cbce50647e4a931fc83da408
dohwe0528@gmail.com <dohwe0528@gmail.com>
2024-04-24 13:44:31 +0900

    modified pollingproc.go

--- src/kaosendrequest/pollingproc.go
+++ src/kaosendrequest/pollingproc.go
@@ -67,149 +67,70 @@ func resPollingProcess(wg *sync.WaitGroup) {
 		
 	
 		if len(supmsgids) >= 1000 {
-
-			var commastr = "update DHN_RESULT set result = 'Y' where sync = 'N' and msgid in ("
-
-			for i := 1; i < len(supmsgids); i++ {
-				commastr = commastr + "?,"
-			}
-
-			commastr = commastr + "?)"
-
-			_, err1 := db.Exec(commastr, supmsgids...)
-
-			if err1 != nil {
-				errlog.Println("Result Table S Update 처리 중 오류 발생 ")
-			}
-			
-			commastr = "delete from DHN_POLLING_RESULT where msg_id in ("
-			for i := 1; i < len(supmsgids); i++ {
-				commastr = commastr + "?,"
-			}
-			commastr = commastr + "?)"
-			_, err1 = db.Exec(commastr, supmsgids...)
-			if err1 != nil {
-				errlog.Println("Result Table S Delete 처리 중 오류 발생 ")
-			}
-			
+			insDelResData(supmsgids, "S")
 			supmsgids = nil
 		}
 
 		if len(fupmsgids) >= 1000 {
-
-			var commastr = "update DHN_RESULT set result = 'Y',code = '9999', message = 'ME09' where sync = 'N' and msgid in ("
-			for i := 1; i < len(fupmsgids); i++ {
-				commastr = commastr + "?,"
-			}
-			commastr = commastr + "?)"
-			_, err1 := db.Exec(commastr, fupmsgids...)
-			if err1 != nil {
-				errlog.Println("Result Table F Update 처리 중 오류 발생 ")
-			}
-
-			commastr = "delete from DHN_POLLING_RESULT where msg_id in ("
-			for i := 1; i < len(fupmsgids); i++ {
-				commastr = commastr + "?,"
-			}
-			commastr = commastr + "?)"
-			_, err1 = db.Exec(commastr, fupmsgids...)
-			if err1 != nil {
-				errlog.Println("Result Table F Delete 처리 중 오류 발생 ")
-			}
+			insDelResData(supmsgids, "F")
 			fupmsgids = nil
 		}
 	
 	}
 
 	if len(supmsgids) > 0 {
-
-		var commastr = "update DHN_RESULT set result = 'Y' where sync = 'N' and msgid in ("
-	
-		for i := 1; i < len(supmsgids); i++ {
-			commastr = commastr + "?,"
-		}
-	
-		commastr = commastr + "?)"
-	
-		_, err1 := db.Exec(commastr, supmsgids...)
-	
-		if err1 != nil {
-			errlog.Println("Result Table S Update 처리 중 오류 발생 ")
-		}
-		
-		commastr = "delete from DHN_POLLING_RESULT where msg_id in ("
-		for i := 1; i < len(supmsgids); i++ {
-			commastr = commastr + "?,"
-		}
-		commastr = commastr + "?)"
-		_, err1 = db.Exec(commastr, supmsgids...)
-		if err1 != nil {
-			errlog.Println("Result Table S Delete 처리 중 오류 발생 ")
-		}
- 
-		supmsgids = nil
+		insDelResData(supmsgids, "S")
 	}
 	
 	if len(fupmsgids) > 0 {
-	
-		var commastr = "update DHN_RESULT set result = 'Y',code = '9999', message = 'ME09' where sync = 'N' and msgid in ("
-	
-		for i := 1; i < len(fupmsgids); i++ {
-			commastr = commastr + "?,"
-		}
-	
-		commastr = commastr + "?)"
-	
-		_, err1 := db.Exec(commastr, fupmsgids...)
-	
-		if err1 != nil {
-			errlog.Println("Result Table F Update 처리 중 오류 발생 ")
-		}
-	
-		commastr = "delete from DHN_POLLING_RESULT where msg_id in ("
-		for i := 1; i < len(fupmsgids); i++ {
-			commastr = commastr + "?,"
-		}
-		commastr = commastr + "?)"
-		_, err1 = db.Exec(commastr, fupmsgids...)
-		if err1 != nil {
-			errlog.Println("Result Table F Delete 처리 중 오류 발생 ")
-		}
-		fupmsgids = nil 
+		insDelResData(supmsgids, "F")
 	}
 	  
 }
 
-func proto(idValues []interface, res string){
+func insDelResData(idValues []interface, res string){
 	tx, err := databasepool.DB.Begin()
 	if err != nil {
-		confiag.Stdlog.Println("polling_proc.go / getPollingProcess / dhn_polling_result / 트랜젝션 초기화 실패 ", err)
+		confiag.Stdlog.Println("polling_proc.go / getPollingProcess / dhn_result / 트랜젝션 초기화 실패 ", err)
 	}
 	defer tx.Rollback()
 
-	var stmt
+	var stmt *sql.Stmt
 	if res == "S" {
 		stmt, err = tx.Prepare("update DHN_RESULT set result = 'Y' where sync = 'N' and msgid = $1")
 	} else {
 		stmt, err = tx.Prepare("update DHN_RESULT set result = 'Y',code = '9999', message = 'ME09' where sync = 'N' and msgid = $1")
 	}
+
+	if err != nil {
+		confiag.Stdlog.Println("polling_proc.go / getPollingProcess / dhn_result / stmt insert 초기화 실패 ", err)
+		return
+	}
+
+	stmt2, err := tx.Prepare("delete from DHN_POLLING_RESULT where msg_id = $1")
 	
 	if err != nil {
-		confiag.Stdlog.Println("getpolling.go / getPollingProcess / dhn_polling_result / ftStmt 초기화 실패 ", err)
+		confiag.Stdlog.Println("polling_proc.go / getPollingProcess / dhn_result / delete stmt 초기화 실패 ", err)
+		stmt.Close()
 		return
 	}
 
 	for _, data := range idValues {
-		_, err := atStmt.Exec(data)
+		_, err := stmt.Exec(data)
 		if err != nil {
-			confiag.Stdlog.Println("getpolling.go / getPollingProcess / dhn_polling_result / ftStmt personal Exec ", err)
+			confiag.Stdlog.Println("polling_proc.go / getPollingProcess / dhn_result / stmt insert Exec ", err)
+		}
+		_, err = stmt2.Exec(data)
+		if err != nil {
+			confiag.Stdlog.Println("polling_proc.go / getPollingProcess / dhn_result / stmt delete Exec ", err)
 		}
 	}
-	
+
 	stmt.Close()
+	stmt2.Close()
 
 	err = tx.Commit()
 	if err != nil {
-		confiag.Stdlog.Println("getpolling.go / getPollingProcess / dhn_polling_result / ftStmt commit ", err)
+		confiag.Stdlog.Println("polling_proc.go / getPollingProcess / dhn_result / ftStmt commit ", err)
 	}
 }
