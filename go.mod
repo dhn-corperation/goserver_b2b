4f96e8042c22369e2d670cfa4d53b17d5a14d019
dhpj <dohwe0528@gmail.com>
2024-05-02 09:24:29 +0900

    v1

--- go.mod
+++ go.mod
@@ -30,6 +30,7 @@ require (
 	github.com/leodido/go-urn v1.2.4 // indirect
 	github.com/lestrrat/go-envload v0.0.0-20180220120943-6ed08b54a570 // indirect
 	github.com/lestrrat/go-strftime v0.0.0-20180220042222-ba3bf9c1d042 // indirect
+	github.com/lib/pq v1.10.9 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
--- src/kaocenter/dhncenter.go
+++ src/kaocenter/dhncenter.go
@@ -1,22 +1,25 @@
 package kaocenter
 
 import (
+	"database/sql"
 	config "mycs/src/kaoconfig"
 	db "mycs/src/kaodatabasepool"
-	"database/sql"
+
 	"github.com/gin-gonic/gin"
 )
 
-func Get_crypto(c *gin.Context){
+func Get_crypto(c *gin.Context) {
 	userid := c.Request.Header.Get("userid")
 	userip := c.ClientIP()
 	var crypto sql.NullString
-	err := db.DB.QueryRow("select crypto from DHN_CLIENT_LIST where user_id = '"+userid+"' and ip = '"+userip+"'").Scan(&crypto)
-	if err != nil { config.Stdlog.Println(err) }
+	err := db.DB.QueryRow("select crypto from DHN_CLIENT_LIST where user_id = '" + userid + "' and ip = '" + userip + "'").Scan(&crypto)
+	if err != nil {
+		config.Stdlog.Println(err)
+	}
 	if crypto.Valid {
 		c.String(200, crypto.String)
 	} else {
 		c.String(404, "")
 	}
-	
+
 }
