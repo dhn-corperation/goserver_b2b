56b767470e82d3dec384d04727b21708b42488e8
dohwe0528@gmail.com <dohwe0528@gmail.com>
2024-05-16 11:30:07 +0900

    v1

--- src/oshotproc/oshotproc.go
+++ src/oshotproc/oshotproc.go
@@ -209,8 +209,8 @@ func resProcess(ctx context.Context, group_no string, user_id string) {
 			ml := len(ms)
 
 			if s.EqualFold(sms_kind.String, "S") {
-				ms = []rune(string(ms))
-				ms = string(ms[:100])
+				runeMs = []rune(ms)
+				ms = string(runeMs[:100])
 				if ml <= 90 || s.EqualFold(sms_len_check.String, "N") {
 					smsValues = append(smsValues, kaocommon.OshotReqColumn{
 						Sender : sms_sender.String,
