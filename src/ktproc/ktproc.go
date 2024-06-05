eadf42bb91ba11528f45cd88deb16e872711c243
dohwe0528@gmail.com <dohwe0528@gmail.com>
2024-06-05 11:19:20 +0900

    add KT

--- src/ktproc/ktproc.go
+++ src/ktproc/ktproc.go
@@ -49,7 +49,6 @@ func KtProcess(user_id string, ctx context.Context, acc int) {
 				limit 1
 					`
 				cnterr := databasepool.DB.QueryRowContext(ctx, tickSql, user_id).Scan(&count)
-				config.Stdlog.Println("asdf?")
 
 				if cnterr != nil && cnterr != sql.ErrNoRows {
 					config.Stdlog.Println("DHN_RESULT Table - select 오류 : " + cnterr.Error())
@@ -185,8 +184,11 @@ func resProcess(ctx context.Context, group_no string, user_id string, acc int) {
 	reg, err := regexp.Compile("[^0-9]+")
 	smsSeq := 1
 	mmsSeq := 1
+
+	stdlog.Println("여기1")
 	
 	for resrows.Next() {
+		stdlog.Println("여기2")
 		resrows.Scan(&msgid, &code, &message, &message_type, &msg_sms, &phn, &remark1, &remark2, &result, &sms_lms_tit, &sms_kind, &sms_sender, &res_dt, &reserve_dt, &mms_file1, &mms_file2, &mms_file3, &msgLen, &userid, &sms_len_check)
 		phnstr = phn.String
 
@@ -245,6 +247,7 @@ func resProcess(ctx context.Context, group_no string, user_id string, acc int) {
 					db.Exec("update DHN_RESULT dr set dr.result = 'Y', dr.code = '7003', dr.message = '메세지 길이 오류', dr.remark2 = date_format(now(), '%Y-%m-%d %H:%i:%S') where userid = '" + userid.String + "' and msgid = '" + msgid.String + "'")
 				}
 			} else if s.EqualFold(sms_kind.String, "L") || s.EqualFold(sms_kind.String, "M") {
+				stdlog.Println("여기3")
 				mmsBox = SendReqTable{
 					MessageSubType : 1,
 					CallbackNumber : sms_sender.String,
@@ -308,6 +311,7 @@ func resProcess(ctx context.Context, group_no string, user_id string, acc int) {
 
 				mmsSeq++
 				lmscnt++
+				stdlog.Println("여기4")
 			}
 
 		} else {
