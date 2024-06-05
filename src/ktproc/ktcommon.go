d89578c74266e6a8c47f030b8d010e278fe9544c
mango7431 <rsm5065@naver.com>
2024-06-05 09:45:33 +0900

    add code

--- src/ktproc/ktcommon.go
+++ src/ktproc/ktcommon.go
@@ -3,8 +3,8 @@ package ktproc
 import (
 	"bytes"
 	"crypto/hmac"
-	"crypto/sha256"
 	"crypto/md5"
+	"crypto/sha256"
 	"encoding/hex"
 	"encoding/json"
 	"io/ioutil"
@@ -13,7 +13,6 @@ import (
 	"os"
 	"strings"
 	"time"
-	
 	// config "mycs/src/kaoconfig"
 )
 
@@ -23,19 +22,19 @@ const (
 )
 
 type Message struct {
-	apiKey string
-	apiPw string
+	apiKey  string
+	apiPw   string
 	userKey string
-	isDev bool
+	isDev   bool
 	apiHost string
 }
 
 func NewMessage(apiKey, apiPw, userKey string, isDev bool, apiCenter int) *Message {
 	m := &Message{
-		apiKey: apiKey,
-		apiPw: apiPw,
+		apiKey:  apiKey,
+		apiPw:   apiPw,
 		userKey: userKey,
-		isDev: isDev,
+		isDev:   isDev,
 	}
 	switch apiCenter {
 	case 1:
@@ -53,7 +52,7 @@ func (m *Message) setHeader(param interface{}, isMulti bool) ([]string, string) 
 	datetime := time.Now().Format("20060102150405")
 	hashKey := m.apiPw + "_" + datetime
 	hashData, _ := json.Marshal(param)
-	hash := m.getHash(string(hashData) + m.userKey, hashKey)
+	hash := m.getHash(string(hashData)+m.userKey, hashKey)
 
 	headers := []string{
 		"API-KEY: " + m.apiKey,
@@ -151,7 +150,7 @@ func (m *Message) ExecMMS(apiUrl string, param SendReqTable, fileParam []string)
 func (m *Message) requestAPI(apiUrl string, headers []string, body []byte) (*http.Response, error) {
 	client := &http.Client{}
 	fullUrl := apiUrl
-	if !m.isDev{
+	if !m.isDev {
 		fullUrl = m.apiHost + apiUrl
 	} else {
 		fullUrl = URL_DEV + apiUrl
@@ -170,4 +169,97 @@ func (m *Message) requestAPI(apiUrl string, headers []string, body []byte) (*htt
 	}
 
 	return client.Do(req)
+}
+
+func KTCode(code string) string {
+	errmsg := map[string]string{
+		"10000": "7006",
+		"10001": "7099",
+		"10002": "7011",
+		"10003": "703",
+		"10004": "7011",
+		"10005": "7011",
+		"10006": "7011",
+		"10007": "7011",
+		"10008": "7011",
+		"10009": "7011",
+		"10010": "7011",
+		"10011": "7011",
+		"10012": "7011",
+		"10013": "7011",
+		"10014": "7011",
+		"10015": "7011",
+		"10016": "7011",
+		"10017": "7011",
+		"10018": "7011",
+		"10019": "7011",
+		"10020": "7011",
+		"10021": "7060",
+		"10022": "7011",
+		"10023": "7011",
+		"10024": "7011",
+		"10025": "7011",
+		"10026": "7011",
+		"10027": "7011",
+		"10028": "7011",
+		"10029": "7011",
+		"10030": "7011",
+		"10031": "7011",
+		"10032": "7011",
+		"10033": "7093",
+		"10034": "7061",
+		"10035": "7036",
+		"10036": "7011",
+		"10037": "7011",
+		"10038": "7011",
+		"10039": "7098",
+		"10050": "7011",
+		"10100": "7011",
+		"10101": "7015",
+		"10102": "7013",
+		"10103": "7014",
+		"10104": "7014",
+		"10105": "7061",
+		"10106": "7061",
+		"10107": "7022",
+		"10108": "7011",
+		"10109": "7016",
+		"10110": "7011",
+		"10111": "7084",
+		"10112": "7011",
+		"10113": "7011",
+		"10114": "7053",
+		"10115": "7052",
+		"10116": "7051",
+		"10117": "7051",
+		"10200": "7023",
+		"10201": "7023",
+		"10202": "7059",
+		"10203": "7021",
+		"10212": "7021",
+		"10253": "7021",
+		"12002": "7059",
+		"12003": "7059",
+		"12107": "7059",
+		"14005": "7059",
+		"14007": "7059",
+		"14301": "7036",
+		"14307": "7036",
+		"20000": "7059",
+		"40000": "7097",
+		"40002": "7099",
+		"40003": "7099",
+		"41000": "7099",
+		"41001": "7099",
+		"41002": "7097",
+		"41003": "7097",
+		"42000": "7099",
+		"42001": "7095",
+		"50000": "7099",
+	}
+	val, ex := errmsg[code]
+	if !ex {
+		val = "7011"
+	}
+	return val
 }
--- src/ktproc/ktmms.go
+++ src/ktproc/ktmms.go
@@ -2,51 +2,52 @@ package ktproc
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	config "mycs/src/kaoconfig"
 	databasepool "mycs/src/kaodatabasepool"
 	"strconv"
-	"io/ioutil"
-	"encoding/json"
 
 	//	"log"
 	s "strings"
 	"sync"
 	"time"
 
-	_ "github.com/go-sql-driver/mysql"
 	"context"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func LMSProcess(ctx context.Context, acc int) {
 	var wg sync.WaitGroup
 
 	for {
-			select {
-				case <- ctx.Done():
-			
-			    config.Stdlog.Println("Oshot MMS process가 20초 후에 종료 됨.")
-			    time.Sleep(20 * time.Second)
-			    config.Stdlog.Println("Oshot MMS process 종료 완료")
-			    return
-			default:	
-			
-				var t = time.Now()
-	
-				if t.Day() < 3 {
-					for i:=1;i<6;i++ {
-						wg.Add(1)
-						go mmsProcess(&wg, "KT_MMS", true, i, acc)
-					}
-				}
-				
-				for i:=1;i<6;i++ {
-					wg.Add(1)
-					go mmsProcess(&wg, "KT_MMS", false, i, acc)
-				}
+		select {
+		case <-ctx.Done():
 
-				wg.Wait()
+			config.Stdlog.Println("Oshot MMS process가 20초 후에 종료 됨.")
+			time.Sleep(20 * time.Second)
+			config.Stdlog.Println("Oshot MMS process 종료 완료")
+			return
+		default:
+
+			var t = time.Now()
+
+			if t.Day() < 3 {
+				for i := 1; i < 6; i++ {
+					wg.Add(1)
+					go mmsProcess(&wg, "KT_MMS", true, i, acc)
+				}
 			}
+
+			for i := 1; i < 6; i++ {
+				wg.Add(1)
+				go mmsProcess(&wg, "KT_MMS", false, i, acc)
+			}
+
+			wg.Wait()
+		}
 	}
 
 }
@@ -96,10 +97,10 @@ func mmsProcess(wg *sync.WaitGroup, table string, preFlag bool, seq int, acc int
 			searchData.Scan(&userid, &msgid, &resp_JobID)
 
 			sendData := SearchReqTable{
-				JobIDs : []int64{
+				JobIDs: []int64{
 					resp_JobID.Int64,
 				},
-				SendDay : time.Now().Format("20060102"),
+				SendDay: time.Now().Format("20060102"),
 			}
 
 			resp, err := client.SearchResult("/inquiry/report/", sendData)
@@ -128,7 +129,8 @@ func mmsProcess(wg *sync.WaitGroup, table string, preFlag bool, seq int, acc int
 				continue
 			}
 
-			//TODO 결과코드 변환 과정 필요함
+			convResult := strconv.Itoa(first.Result)
+			resultCode := KTCode(convResult)
 
 			var telInfo = "ETC"
 			var telInfoLog = 0
@@ -143,36 +145,35 @@ func mmsProcess(wg *sync.WaitGroup, table string, preFlag bool, seq int, acc int
 				telInfoLog = *first.TelconInfo
 			}
 
-		    parsedTime, err := time.Parse("20060102150405", first.Time)
-		    if err != nil {
-		        errlog.Println(userid.String, "- msgid : ", msgid.String, " KT크로샷 결과조회 API 발송 중 시간변환 오류 발생 : ", err, "  /  statusCode : ", first.Time)
-		        continue
-		    }
+			parsedTime, err := time.Parse("20060102150405", first.Time)
+			if err != nil {
+				errlog.Println(userid.String, "- msgid : ", msgid.String, " KT크로샷 결과조회 API 발송 중 시간변환 오류 발생 : ", err, "  /  statusCode : ", first.Time)
+				continue
+			}
 
-		    formattedTime := parsedTime.Format("2006-01-02 15:04:05")
-
+			formattedTime := parsedTime.Format("2006-01-02 15:04:05")
 
 			_, err = db.Exec(`insert into ` + MMSTable + `(userid, msgid, MessageSubType, CallbackNumber, Bundle_Seq, Bundle_Num, Bundle_Content, Bundle_Subject, Image_path1, Image_path2, Image_path3, resp_JobID, resp_Time, resp_SubmitTime, resp_Result, Resp_TelconInfo, resp_EndUserID, resp_ServiceProviderID, sep_seq, dhn_id)
-				select userid, msgid, MessageSubType, CallbackNumber, Bundle_Seq, Bundle_Num, Bundle_Content, Bundle_Subject, Image_path1, Image_path2, Image_path3, resp_JobID, '`+first.Time+`', '`+first.SubmitTime+`', '`+strconv.Itoa(first.Result)+`', '`+strconv.Itoa(telInfoLog)+`', '`+first.EndUserID+`', '`+first.ServiceProviderID+`', sep_seq, dhn_id
+				select userid, msgid, MessageSubType, CallbackNumber, Bundle_Seq, Bundle_Num, Bundle_Content, Bundle_Subject, Image_path1, Image_path2, Image_path3, resp_JobID, '` + first.Time + `', '` + first.SubmitTime + `', '` + strconv.Itoa(first.Result) + `', '` + strconv.Itoa(telInfoLog) + `', '` + first.EndUserID + `', '` + first.ServiceProviderID + `', sep_seq, dhn_id
 				from KT_MMS
-				WHERE userid = '`+userid.String+`' and msgid = '`+msgid.String+`'`)
+				WHERE userid = '` + userid.String + `' and msgid = '` + msgid.String + `'`)
 			if err != nil {
 				errlog.Println(userid.String, "- msgid : ", msgid.String, " KT크로샷 결과조 API 결과 LOG 테이블 입력중 에러 발생 : ", err)
 				continue
 			}
 			if first.Result != 10000 {
-				_, err = db.Exec("update DHN_RESULT set message_type = 'PH', result = 'Y', code = '" + strconv.Itoa(first.Result) + "', message = concat(dr.message, '," + "여기 에러 문구 넣어야" + "'), remark1 = '" + telInfo + "', dr.remark2 = '" + formattedTime + "' where userid='" + userid.String + "' and msgid = '" + msgid.String+ "'")
+				_, err = db.Exec("update DHN_RESULT set message_type = 'PH', result = 'Y', code = '" + resultCode + "', message = concat(dr.message, '," + "여기 에러 문구 넣어야" + "'), remark1 = '" + telInfo + "', dr.remark2 = '" + formattedTime + "' where userid='" + userid.String + "' and msgid = '" + msgid.String + "'")
 				if err != nil {
 					errlog.Println(userid.String, "- msgid : ", msgid.String, " KT크로샷 결과조 API 결과 DHN_RESULT 테이블 반영 실패1 : ", err)
 				}
 			} else {
-				_, err = db.Exec("update DHN_RESULT set message_type = 'PH', result = 'Y', code = '0000', message = '', remark1 = '" + telInfo + "', dr.remark2 = '" + formattedTime + "' where userid='" + userid.String + "' and msgid = '" + msgid.String+ "'")
+				_, err = db.Exec("update DHN_RESULT set message_type = 'PH', result = 'Y', code = '0000', message = '', remark1 = '" + telInfo + "', dr.remark2 = '" + formattedTime + "' where userid='" + userid.String + "' and msgid = '" + msgid.String + "'")
 				if err != nil {
 					errlog.Println(userid.String, "- msgid : ", msgid.String, " KT크로샷 결과조 API 결과 DHN_RESULT 테이블 반영 실패2 : ", err)
 				}
 			}
-			
-			db.Exec(`delete from KT_MMS where userid = '`+userid.String+`' and msgid = '`+msgid.String+`'`)
+
+			db.Exec(`delete from KT_MMS where userid = '` + userid.String + `' and msgid = '` + msgid.String + `'`)
 
 		}
 	}
