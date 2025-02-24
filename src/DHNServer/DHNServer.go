e13c6cac62e2817a5b06ecb37bca44de1fa768bb
dohwe0528@gmail.com <dohwe0528@gmail.com>
2025-02-24 10:09:03 +0900

    remove nano, kt, add api

--- src/DHNServer/DHNServer.go
+++ src/DHNServer/DHNServer.go
@@ -6,20 +6,15 @@ import (
 	"sort"
 	"syscall"
 	"context"
-	"strconv"
 	"os/signal"
 	"database/sql"
 	s "strings"
 
-
 	"mycs/src/kaosendrequest"
-	"mycs/src/nanoproc"
 	"mycs/src/oshotproc"
-	"mycs/src/ktproc"
 	"mycs/src/lguproc"
 	"mycs/src/otpatproc"
 	"mycs/src/otplguproc"
-	"mycs/src/otpnanoproc"
 	"mycs/src/otposhotproc"
 	config "mycs/src/kaoconfig"
 	databasepool "mycs/src/kaodatabasepool"
@@ -242,128 +237,6 @@ func resultProc() {
 	allCtxC["oshotsms"] = oscancel
 	allService["oshotsms"] = "Oshot SMS"
 
-	ktxroUser := map[string]string{}
-	ktxroCtxC := map[string]interface{}{}
-
-	ktxroUserList, error := databasepool.DB.Query("select distinct user_id from DHN_CLIENT_LIST dcl where dcl.use_flag = 'Y' and upper(ifnull(dcl.dest, '')) = 'KTXRO'")
-	isKtxro := true
-	if error != nil {
-		config.Stdlog.Println("KT 크로샷 유저 select 오류 ")
-		isKtxro = false
-	}
-	defer ktxroUserList.Close()
-
-	if isKtxro {
-		var user_id sql.NullString
-
-		for ktxroUserList.Next() {
-			ktxroUserList.Scan(&user_id)
-			ctx, cancel := context.WithCancel(context.Background())
-			ctx = context.WithValue(ctx, "user_id", user_id.String)
-			go ktproc.KtProcess(user_id.String, ctx, 0)
-
-			ktxroUser[user_id.String] = user_id.String
-			ktxroCtxC[user_id.String] = cancel
-
-			allCtxC["KTX"+user_id.String] = cancel
-			allService["KTX"+user_id.String] = user_id.String
-
-		}
-	}
-
-	klctx, klcancel := context.WithCancel(context.Background())
-	go ktproc.LMSProcess(klctx, 0)
-	allCtxC["ktxrolms"] = klcancel
-	allService["ktxrolms"] = "Ktxro LMS"
-
-	Ksctx, Kscancel := context.WithCancel(context.Background())
-	go ktproc.SMSProcess(Ksctx, 0)
-	allCtxC["ktxrosms"] = Kscancel
-	allService["ktxrosms"] = "Ktxro SMS"
-
-	nanoUser := map[string]string{}
-	nanoCtxC := map[string]interface{}{}
-
-	nanoUserList, error := databasepool.DB.Query("select distinct user_id, nano_tel_seperate from DHN_CLIENT_LIST dcl where dcl.use_flag = 'Y' and upper(ifnull(dcl.dest, '')) = 'NANO'")
-	isNano := true
-	if error != nil {
-		config.Stdlog.Println("나노 유저 select 오류 ")
-		isNano = false
-	}
-	defer nanoUserList.Close()
-
-	if isNano {
-		var user_id sql.NullString
-		var nano_tel_seperate sql.NullString
-
-		for nanoUserList.Next() {
-			nanoUserList.Scan(&user_id, &nano_tel_seperate)
-
-			if s.EqualFold(nano_tel_seperate.String, "N") { // 기본
-				ctx, cancel := context.WithCancel(context.Background())
-				ctx = context.WithValue(ctx, "user_id", user_id.String)
-
-				go nanoproc.NanoProcess(user_id.String, ctx)
-
-				nanoUser[user_id.String] = user_id.String
-				nanoCtxC[user_id.String] = cancel
-
-				allCtxC["NN"+user_id.String] = cancel
-				allService["NN"+user_id.String] = user_id.String
-			} else if s.EqualFold(nano_tel_seperate.String, "Y") { // 저가
-
-				ctxY, cancelY := context.WithCancel(context.Background())
-				ctxY = context.WithValue(ctxY, "user_id", user_id.String)
-
-				ctxN, cancelN := context.WithCancel(context.Background())
-				ctxN = context.WithValue(ctxN, "user_id", user_id.String)
-
-				go nanoproc.NanoProcess_Y(user_id.String, ctxY) // 010으로 시작하는 번호
-				go nanoproc.NanoProcess_N(user_id.String, ctxN) // 010이 아닌 번호
-
-				nanoUser[user_id.String+"_Y"] = user_id.String
-				nanoCtxC[user_id.String+"_Y"] = cancelY
-				allCtxC["NN"+user_id.String+"_Y"] = cancelY
-				allService["NN"+user_id.String+"_Y"] = "NanoService Y"
-
-				nanoUser[user_id.String+"_N"] = user_id.String
-				nanoCtxC[user_id.String+"_N"] = cancelN
-				allCtxC["NN"+user_id.String+"_N"] = cancelN
-				allService["NN"+user_id.String+"_N"] = "NanoService N"
-			}
-
-		}
-
-	}
-
-	nlctx, nlcancel := context.WithCancel(context.Background())
-
-	go nanoproc.NanoLMSProcess(nlctx)
-
-	allCtxC["nanolms"] = nlcancel
-	allService["nanolms"] = "Nano LMS"
-
-	nsctx, nscancel := context.WithCancel(context.Background())
-
-	go nanoproc.NanoSMSProcess(nsctx)
-
-	allCtxC["nanosms"] = nscancel
-	allService["nanosms"] = "Nano SMS"
-
-	nlctxG, nlcancelG := context.WithCancel(context.Background())
-
-	go nanoproc.NanoLMSProcess_G(nlctxG)
-
-	allCtxC["nanolmsG"] = nlcancelG
-	allService["nanolmsG"] = "Nano LMS G"
-
-	nsctxG, nscancelG := context.WithCancel(context.Background())
-
-	go nanoproc.NanoSMSProcess_G(nsctxG)
-
-	allCtxC["nanosmsG"] = nscancelG
-	allService["nanosmsG"] = "Nano SMS G"
-
 	lguUser := map[string]string{}
 	lguCtxC := map[string]interface{}{}
 
@@ -409,8 +282,6 @@ func resultProc() {
 	otpAtCtxC := map[string]interface{}{} 
 	otpLguUser := map[string]string{}
 	otpLguCtxC := map[string]interface{}{}
-	otpNanoUser := map[string]string{}
-	otpNanoCtxC := map[string]interface{}{}
 	otpOshotUser := map[string]string{}
 	otpOshotCtxC := map[string]interface{}{}
 
@@ -422,15 +293,6 @@ func resultProc() {
 
 	allCtxC["OTPAT"] = cancel
 	allService["OTPAT"] = "OTPAT"
-
-	// ctx, cancel := context.WithCancel(context.Background())
-	// go otpnanoproc.NanoProcess(ctx)
-
-	// otpNanoUser["OTPNANO"] = "OTPNANO"
-	// otpNanoCtxC["OTPNANO"] = cancel
-
-	// allCtxC["OTPNANO"] = cancel
-	// allService["OTPNANO"] = "OTPNANO"
 
 	ctx, cancel := context.WithCancel(context.Background())
 	go otposhotproc.OshotProcess(ctx)
@@ -451,16 +313,6 @@ func resultProc() {
 	allCtxC["otplgusms"] = olscancel
 	allService["otplgusms"] = "LGU OTP SMS"
 
-	onlctx, onlcancel := context.WithCancel(context.Background())
-	go otpnanoproc.LMSProcess(onlctx)
-	allCtxC["otpnanolms"] = onlcancel
-	allService["otpnanolms"] = "NANO OTP LMS"
-
-	onsctx, onscancel := context.WithCancel(context.Background())
-	go otpnanoproc.SMSProcess(onsctx)
-	allCtxC["otpnanosms"] = onscancel
-	allService["otpnanosms"] = "NANO OTP SMS"
-
 	oomctx, oomcancel := context.WithCancel(context.Background())
 	go otposhotproc.MSGProcess(oomctx)
 	allCtxC["otposhotmsg"] = oomcancel
@@ -477,13 +329,9 @@ Command :
 /orun?uid=dhn    	 	-> dhn Oshot process run.
 /olist           	 	-> 실행 중인 Oshot process User List.
 
-/nstop?uid=dhn   	 	-> dhn Nano process stop.
-/nrun?uid=dhn        	-> dhn Nano process run.
-/nlist               	-> 실행 중인 Nano process User List.
-
-/kstop?uid=dhn       	-> dhn KTXRO process stop.
-/krun?uid=dhn&acc=0  	-> dhn KTXRO process run.
-/klist               	-> 실행 중인 KTXRO process User List.
+/nostop?uid=dhn   	 	-> dhn NOshot process stop.
+/norun?uid=dhn    	 	-> dhn NOshot process run.
+/nolist           	 	-> 실행 중인 NOshot process User List.
 
 /lgstop?uid=dhn   	 	-> dhn Lgu process stop.
 /lgrun?uid=dhn   	 	-> dhn Lgu process run.
@@ -554,43 +402,17 @@ Command :
 		c.String(200, key)
 	})
 
-	r.GET("/nstop", func(c *gin.Context) {
+	r.GET("/nostop", func(c *gin.Context) {
 		var uid string
 		uid = c.Query("uid")
-		temp := nanoCtxC[uid]
+		temp := oshotCtxC[uid]
 		if temp != nil {
-			var nano_tel_seperate sql.NullString
-			var nts string
-
-			seperr := databasepool.DB.QueryRow("select distinct nano_tel_seperate from DHN_CLIENT_LIST where user_id = ?", uid).Scan(&nano_tel_seperate)
-			if seperr != nil && seperr != sql.ErrNoRows {
-				config.Stdlog.Println(uid," /nrun 나노 nano_tel_seperate 습득 실패 : ", seperr)
-				nts = "N"
-			} else {
-				nts = nano_tel_seperate.String
-			}
-
-			cancel := nanoCtxC[uid].(context.CancelFunc)
+			cancel := oshotCtxC[uid].(context.CancelFunc)
 			cancel()
-			
-			if s.EqualFold(nts, "N") { // 기본
-				delete(nanoCtxC, uid)
-				delete(nanoUser, uid)
-
-				delete(allService, "NN"+uid)
-				delete(allCtxC, "NN"+uid)
-			} else if s.EqualFold(nts, "Y") { // 콜비서
-				delete(nanoCtxC, uid+"_Y")
-				delete(nanoUser, uid+"_Y")
-				delete(allService, "NN"+uid+"_Y")
-				delete(allCtxC, "NN"+uid+"_Y")
-
-				delete(nanoCtxC, uid+"_N")
-				delete(nanoUser, uid+"_N")
-				delete(allService, "NN"+uid+"_N")
-				delete(allCtxC, "NN"+uid+"_N")
-			}
-
+			delete(oshotCtxC, uid)
+			delete(oshotUser, uid)
+			delete(allService, "OS"+uid)
+			delete(allCtxC, "OS"+uid)
 			c.String(200, uid+" 종료 신호 전달 완료")
 		} else {
 			c.String(200, uid+"는 실행 중이지 않습니다.")
@@ -598,132 +420,36 @@ Command :
 
 	})
 
-	r.GET("/nrun", func(c *gin.Context) {
+	r.GET("/norun", func(c *gin.Context) {
 		var uid string
 		uid = c.Query("uid")
-		temp := nanoCtxC[uid]
-		if temp != nil {
-			c.String(200, uid+" 이미 실행 중입니다.")
-		} else {
-			var nano_tel_seperate sql.NullString
-			var nts string
-
-			seperr := databasepool.DB.QueryRow("select distinct nano_tel_seperate from DHN_CLIENT_LIST where user_id = ?", uid).Scan(&nano_tel_seperate)
-			if seperr != nil && seperr != sql.ErrNoRows {
-				config.Stdlog.Println(uid," /nrun 나노 nano_tel_seperate 습득 실패 : ", seperr)
-				nts = "N"
-			} else {
-				nts = nano_tel_seperate.String
-			}
-
-
-			if s.EqualFold(nts, "N") { // 기본
-				ctx, cancel := context.WithCancel(context.Background())
-				ctx = context.WithValue(ctx, "user_id", uid)
-
-				go nanoproc.NanoProcess(uid, ctx)
-
-				nanoCtxC[uid] = cancel
-				nanoUser[uid] = uid
-
-				allCtxC["NN"+uid] = cancel
-				allService["NN"+uid] = uid
-			} else if s.EqualFold(nts, "Y") { // 콜비서
-
-				ctxY, cancelY := context.WithCancel(context.Background())
-				ctxY = context.WithValue(ctxY, "user_id", uid)
-
-				ctxN, cancelN := context.WithCancel(context.Background())
-				ctxN = context.WithValue(ctxN, "user_id", uid)
-
-				go nanoproc.NanoProcess_Y(uid, ctxY) // 010으로 시작하는 번호
-				go nanoproc.NanoProcess_N(uid, ctxN) // 010이 아닌 번호
-
-				nanoCtxC[uid+"_Y"] = cancelY
-				nanoUser[uid+"_Y"] = uid
-				allCtxC["NN"+uid+"_Y"] = cancelY
-				allService["NN"+uid+"_Y"] = "NanoService Y"
-
-				nanoCtxC[uid+"_N"] = cancelN
-				nanoUser[uid+"_N"] = uid
-				allCtxC["NN"+uid+"_N"] = cancelN
-				allService["NN"+uid+"_N"] = "NanoService N"
-			}
-
-			_, err := databasepool.DB.Exec("update DHN_CLIENT_LIST set dest = 'NANO' where use_flag = 'Y' and user_id = ?", uid)
-				
-			if err != nil {
-				config.Stdlog.Println(uid," /nrun 나노 DHN_CLIENT_LIST 업데이트 실패 : ", err)
-			}
-
-			c.String(200, uid+" 시작 신호 전달 완료")
-
-		}
-	})
-
-	r.GET("/nlist", func(c *gin.Context) {
-		var key string
-		for k := range nanoUser {
-			key = key + k + "\n"
-		}
-		c.String(200, key)
-	})
-
-	r.GET("/kstop", func(c *gin.Context) {
-		var uid string
-		uid = c.Query("uid")
-		temp := ktxroCtxC[uid]
-		if temp != nil {
-			cancel := ktxroCtxC[uid].(context.CancelFunc)
-			cancel()
-			delete(ktxroCtxC, uid)
-			delete(ktxroUser, uid)
-			delete(allService, "KTX"+uid)
-			delete(allCtxC, "KTX"+uid)
-
-			c.String(200, uid+" 종료 신호 전달 완료")
-		} else {
-			c.String(200, uid+"는 실행 중이지 않습니다.")
-		}
-
-	})
-
-	r.GET("/krun", func(c *gin.Context) {
-		var uid string
-		uid = c.Query("uid")
-		acc := c.Query("acc")
-		convAcc, err := strconv.Atoi(acc)
-		if err != nil {
-			c.String(200, uid+" 에러입니다. err : ", err, "  /  acc : ", acc)
-			return
-		}
-		temp := ktxroCtxC[uid]
+		temp := oshotCtxC[uid]
 		if temp != nil {
 			c.String(200, uid+" 이미 실행 중입니다.")
 		} else {
 			ctx, cancel := context.WithCancel(context.Background())
 			ctx = context.WithValue(ctx, "user_id", uid)
-			go ktproc.KtProcess(uid, ctx, convAcc)
+			go oshotproc.OshotProcess(uid, ctx)
 
-			ktxroCtxC[uid] = cancel
-			ktxroUser[uid] = uid
+			oshotCtxC[uid] = cancel
+			oshotUser[uid] = uid
 
-			allCtxC["KTX"+uid] = cancel
-			allService["KTX"+uid] = uid
+			allCtxC["OS"+uid] = cancel
+			allService["OS"+uid] = uid
 
-			_, err := databasepool.DB.Exec("update DHN_CLIENT_LIST set dest = 'KTXRO' where use_flag = 'Y' and user_id = ?", uid)
+			_, err := databasepool.DB.Exec("update DHN_CLIENT_LIST set dest = 'OSHOT' where use_flag = 'Y' and user_id = ?", uid)
 				
 			if err != nil {
-				config.Stdlog.Println(uid," /krun KT크로샷 DHN_CLIENT_LIST 업데이트 실패 : ", err)
+				config.Stdlog.Println(uid," /orun 오샷 DHN_CLIENT_LIST 업데이트 실패 : ", err)
 			}
 
 			c.String(200, uid+" 시작 신호 전달 완료")
 		}
 	})
 
-	r.GET("/klist", func(c *gin.Context) {
+	r.GET("/nolist", func(c *gin.Context) {
 		var key string
-		for k := range ktxroUser {
+		for k := range oshotUser {
 			key = key + k + "\n"
 		}
 		c.String(200, key)
@@ -795,15 +521,15 @@ Command :
 			delete(allService, "OTPLGU")
 			delete(allCtxC, "OTPLGU")
 		}
-		temp = otpNanoCtxC["OTPNANO"]
+		temp = otpOshotCtxC["OTPOSHOT"]
 		if temp != nil {
-			cancel := otpNanoCtxC["OTPNANO"].(context.CancelFunc)
+			cancel := otpOshotCtxC["OTPOSHOT"].(context.CancelFunc)
 			cancel()
-			delete(otpNanoCtxC, "OTPNANO")
-			delete(otpNanoUser, "OTPNANO")
+			delete(otpOshotCtxC, "OTPOSHOT")
+			delete(otpOshotUser, "OTPOSHOT")
 
-			delete(allService, "OTPNANO")
-			delete(allCtxC, "OTPNANO")
+			delete(allService, "OTPOSHOT")
+			delete(allCtxC, "OTPOSHOT")
 		}
 
 
@@ -856,15 +582,15 @@ Command :
 				c.String(200, "OTP LGU 시작 신호 전달 완료")
 			} else if pf == "nano" {
 				ctx, cancel := context.WithCancel(context.Background())
-				go otpnanoproc.NanoProcess(ctx)
+				go otposhotproc.OshotProcess(ctx)
 
-				otpNanoUser["OTPNANO"] = "OTPNANO"
-				otpNanoCtxC["OTPNANO"] = cancel
+				otpOshotUser["OTPOSHOT"] = "OTPOSHOT"
+				otpOshotCtxC["OTPOSHOT"] = cancel
 
-				allCtxC["OTPNANO"] = cancel
-				allService["OTPNANO"] = "OTPNANO"
+				allCtxC["OTPOSHOT"] = cancel
+				allService["OTPOSHOT"] = "OTPOSHOT"
 
-				c.String(200, "OTP NANO 시작 신호 전달 완료")
+				c.String(200, "OTP OSHOT 시작 신호 전달 완료")
 			}
 			
 		} else if uid == "dhn" && pf == "at" {
@@ -889,7 +615,7 @@ Command :
 			key = key + k + "\n"
 		}
 
-		for l := range otpNanoUser {
+		for l := range otpOshotUser {
 			key = key + l + "\n"
 		}
 		c.String(200, key)
